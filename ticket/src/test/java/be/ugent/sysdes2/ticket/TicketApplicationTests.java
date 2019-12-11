592568c8601305aa1957e9fbf3fe3735a29c5d72
Thomas Devriese <thodvrie.devriese@ugent.be>
2019-12-11 16:53:38 +0100

    Project created

---
+++ ticket/src/test/java/be/ugent/sysdes2/ticket/TicketApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ugent.sysdes2.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
