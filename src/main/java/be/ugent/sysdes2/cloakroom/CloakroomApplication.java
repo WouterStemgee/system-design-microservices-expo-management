0d9167b66d3d7b2d444b408fed82d975eabed8d8
Tarik <tarik.atac@ugent.be>
2019-12-12 12:55:21 +0100

    spingboot init

---
+++ src/main/java/be/ugent/sysdes2/cloakroom/CloakroomApplication.java
@@ -0,0 +1,13 @@
+package be.ugent.sysdes2.cloakroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloakroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloakroomApplication.class, args);
+	}
+
+}
