22394239f7f394cd35109df7d7e48c872185dd82
EliasBout <Elias.bout@ugent.be>
2019-12-16 16:32:25 +0100

    Update SecurityRestController.java

--- src/main/java/be/ugent/sysdev2/security/adapters/rest/SecurityRestController.java
+++ src/main/java/be/ugent/sysdev2/security/adapters/rest/SecurityRestController.java
@@ -1,18 +1,22 @@
 package be.ugent.sysdev2.security.adapters.rest;
 
 import be.ugent.sysdev2.security.adapters.messaging.MessageGateway;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.web.bind.annotation.*;
 import be.ugent.sysdev2.security.domain.Emergency;
 
 
 @RestController
-@RequestMapping("/reception")
+@RequestMapping("/security")
 public class SecurityRestController {
     private MessageGateway mg;
-    private SecurityRestController(MessageGateway mg){
+
+    @Autowired
+    public SecurityRestController(MessageGateway mg){
         this.mg = mg;
     }
+
     @PostMapping("/emergency")
     public String sendEmergency(@RequestBody Emergency emergency){
         mg.resolveEmergency(emergency);
