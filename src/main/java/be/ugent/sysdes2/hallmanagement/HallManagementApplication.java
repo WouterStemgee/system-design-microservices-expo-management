75b7655b4d421bfdfeba151a8f61a77efacfa6e3
Tarik <tarik.atac@ugent.be>
2019-12-17 13:51:26 +0100

    init

---
+++ src/main/java/be/ugent/sysdes2/hallmanagement/HallManagementApplication.java
@@ -0,0 +1,13 @@
+package be.ugent.sysdes2.hallmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HallManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HallManagementApplication.class, args);
+	}
+
+}
