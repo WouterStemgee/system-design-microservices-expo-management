af77397fdd7d71c881f2edeca270f94a6c996b03
Thomas Devriese <thodvrie.devriese@ugent.be>
2019-12-17 22:00:09 +0100

    Project init

---
+++ src/main/java/be/ugent/sysdes2/badge/BadgeApplication.java
@@ -0,0 +1,13 @@
+package be.ugent.sysdes2.badge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadgeApplication.class, args);
+	}
+
+}
