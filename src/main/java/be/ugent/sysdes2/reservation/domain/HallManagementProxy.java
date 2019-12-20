cff926867566482961c6f53e1293f6dbc36ab32a
Wouter Stemgée <wouterstemgee@gmail.com>
2019-12-20 16:26:38 +0100

    HallManagementProxy fix

--- src/main/java/be/ugent/sysdes2/reservation/domain/HallManagementProxy.java
+++ src/main/java/be/ugent/sysdes2/reservation/domain/HallManagementProxy.java
@@ -1,10 +1,12 @@
 package be.ugent.sysdes2.reservation.domain;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -20,31 +22,44 @@ public class HallManagementProxy {
     private String hallManagementUri;
 
     public boolean getAvailability(ZonedDateTime dateStartDate, ZonedDateTime dateEndDate, List<String> halls) {
-        Map<String, String> params = new HashMap<>();
+        RestTemplate restTemplate = new RestTemplate();
+
         String startDate = dateStartDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         String endDate = dateEndDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-        params.put("startDate", startDate);
-        params.put("endDate", endDate);
-        params.put("halls", Arrays.toString(halls.toArray(new String[0])));
 
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<Void> response = restTemplate.getForEntity(hallManagementUri + "availability", Void.class, params);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(hallManagementUri + "availability")
+                .queryParam("startDate", startDate)
+                .queryParam("endDate", endDate)
+                .queryParam("halls", Arrays.toString(halls.toArray(new String[0])));
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, entity, String.class);
         if (response.getStatusCode() == HttpStatus.OK) {
             return true;
         }
         return false;
     }
 
-    public boolean reserve(ZonedDateTime dateStartDate, ZonedDateTime dateEndDate, List<String> halls) {
-        Map<String, String> params = new HashMap<>();
+    public boolean reserve(ZonedDateTime dateStartDate, ZonedDateTime dateEndDate, List<String> halls) throws JSONException {
+        RestTemplate restTemplate = new RestTemplate();
+
         String startDate = dateStartDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         String endDate = dateEndDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-        params.put("startDate", startDate);
-        params.put("endDate", endDate);
-        params.put("halls", Arrays.toString(halls.toArray(new String[0])));
 
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<Void> response = restTemplate.postForEntity(hallManagementUri + "availability", params, Void.class);
+        JSONObject request = new JSONObject();
+        request.put("startDate", startDate);
+        request.put("endDate", endDate);
+        request.put("halls", Arrays.toString(halls.toArray(new String[0])));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<>(request.toString(), headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(hallManagementUri + "reserve", HttpMethod.POST, entity, String.class);
         if (response.getStatusCode() == HttpStatus.OK) {
             return true;
         }
