ec1803fc36dfee97f71325f583f6c74b84c2baec
Wouter Stemgée <wouterstemgee@gmail.com>
2019-12-21 13:15:21 +0100

    Update ReservationRestController.java

--- src/main/java/be/ugent/sysdes2/reservation/adapters/rest/ReservationRestController.java
+++ src/main/java/be/ugent/sysdes2/reservation/adapters/rest/ReservationRestController.java
@@ -1,31 +1,24 @@
 package be.ugent.sysdes2.reservation.adapters.rest;
 
-import be.ugent.sysdes2.reservation.domain.ReservationException;
+
 import be.ugent.sysdes2.reservation.domain.ReservationService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("reservation")
 public class ReservationRestController {
 
@@ -44,13 +37,17 @@ public class ReservationRestController {
     }
 
     @PostMapping("/create")
-    public void createReservation(@RequestBody Map<String, String> payload) throws JsonProcessingException, JSONException {
-        ZonedDateTime startDate = LocalDate.parse(payload.get("startDate"), DateTimeFormatter.ofPattern("dd/MM/yyyy")).atStartOfDay(ZoneId.of("Europe/Brussels"));
-        ZonedDateTime endDate = LocalDate.parse(payload.get("endDate"), DateTimeFormatter.ofPattern("dd/MM/yyyy")).atStartOfDay(ZoneId.of("Europe/Brussels"));
-        int capacity = Integer.parseInt(payload.get("capacity"));
-        List<String> halls = Arrays.asList(new ObjectMapper().readValue(payload.get("halls"), String[].class)); // needs testing with Hall Management
-        int maxVisitors = Integer.parseInt(payload.get("maxVisitors"));
-        float ticketPrice = Float.parseFloat(payload.get("ticketPrice"));
+    public void createReservation(@RequestBody Map<String, Object> payload) throws JsonProcessingException, JSONException {
+        ZonedDateTime startDate = LocalDate.parse((String) payload.get("startDate"), DateTimeFormatter.ofPattern("dd/MM/yyyy")).atStartOfDay(ZoneId.of("Europe/Brussels"));
+        ZonedDateTime endDate = LocalDate.parse((String) payload.get("endDate"), DateTimeFormatter.ofPattern("dd/MM/yyyy")).atStartOfDay(ZoneId.of("Europe/Brussels"));
+        int capacity = Integer.parseInt((String) payload.get("capacity"));
+        JSONArray jsonHalls = new JSONArray(String.valueOf(payload.get("halls")));
+        List<String> halls = new ArrayList<>();
+        for (int i = 0; i < jsonHalls.length(); i++) {
+            halls.add(jsonHalls.getString(i));
+        }
+        int maxVisitors = Integer.parseInt((String) payload.get("maxVisitors"));
+        float ticketPrice = Float.parseFloat((String) payload.get("ticketPrice"));
         reservationService.reserve(startDate, endDate, capacity, halls, maxVisitors, ticketPrice);
     }
 
