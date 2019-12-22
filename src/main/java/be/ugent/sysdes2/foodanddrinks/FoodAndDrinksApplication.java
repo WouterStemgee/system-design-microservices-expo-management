315f450f89c59fe1ad8827fef6119e12b50bbb1b
Thomas Devriese <thodvrie.devriese@ugent.be>
2019-12-22 22:25:03 +0100

    Project init

---
+++ src/main/java/be/ugent/sysdes2/foodanddrinks/FoodAndDrinksApplication.java
@@ -0,0 +1,13 @@
+package be.ugent.sysdes2.foodanddrinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodAndDrinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodAndDrinksApplication.class, args);
+	}
+
+}
