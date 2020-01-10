280695ee10829ed23b0d8f5143df38a739fd4e85
Thomas Devriese <thodvrie.devriese@ugent.be>
2020-01-10 10:21:51 +0100

    Ticket, badge and food-and-drinks methods added

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -116,15 +116,91 @@ export class HttpService {
   updateProgress() {}
 
   // Ticket
-  getTicketAvailability() {}
-  buyTicket() {}
-  validateTicket() {}
+  getTicketAvailability(eventId) {
+    return new Promise((resolve, reject) => {
+      this.http.get(environment.apiGatewayUri + '/ticket/' + eventId + '/availability',{})
+        .subscribe(
+          result => {
+            resolve(result);
+          },
+          (error: HttpErrorResponse) => {
+            reject(error);
+          }
+        );
+    });
+  }
+  buyTicket(eventId, name, numberOfTickets) {
+    return new Promise((resolve, reject) => {
+      this.http.post(environment.apiGatewayUri + '/ticket/buy',
+      {eventId,name,numberOfTickets})
+        .subscribe(
+          result => {
+            resolve(result);
+          },
+          (error: HttpErrorResponse) => {
+            reject(error);
+          }
+        );
+    });
+  }
+  payTicket(eventId, name, numberOfTickets, totalPrice) {
+    return new Promise((resolve, reject) => {
+      this.http.post(environment.apiGatewayUri + '/ticket/pay',
+      {eventId,name,numberOfTickets,totalPrice})
+        .subscribe(
+          result => {
+            resolve(result);
+          },
+          (error: HttpErrorResponse) => {
+            reject(error);
+          }
+        );
+    });
+  }
+  validateTicket(ticketId) {
+    return new Promise((resolve, reject) => {
+      this.http.put(environment.apiGatewayUri + '/ticket/' + ticketId + '/validate',{})
+        .subscribe(
+          result => {
+            resolve(result);
+          },
+          (error: HttpErrorResponse) => {
+            reject(error);
+          }
+        );
+    });
+  }
 
   // Badge
-  rechargeBadge() {}
+  rechargeBadge(badgeId,amount) {
+    return new Promise((resolve, reject) => {
+      this.http.put(environment.apiGatewayUri + '/badge/' + badgeId + '/recharge',{amount})
+        .subscribe(
+          result => {
+            resolve(result);
+          },
+          (error: HttpErrorResponse) => {
+            reject(error);
+          }
+        );
+    });
+  }
 
   // Food and Drinks
-  createOrder() {}
+  createOrder(eventId, badgeId, lineItems) {
+    return new Promise((resolve, reject) => {
+      this.http.post(environment.apiGatewayUri + '/food_and_drinks',
+      {eventId,badgeId,lineItems})
+        .subscribe(
+          result => {
+            resolve(result);
+          },
+          (error: HttpErrorResponse) => {
+            reject(error);
+          }
+        );
+    });
+  }
 
   // Cloakroom
   addCloakroomItem() {}
