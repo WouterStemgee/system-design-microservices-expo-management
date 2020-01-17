047a154ffedd0dcdcfb7ff6b7c6e147a9c72917e
Tarik <tarik.atac@ugent.be>
2020-01-17 21:22:38 +0100

    crossorigin

--- src/main/java/be/ugent/sysdes2/tracking/adapters/rest/TrackingRestController.java
+++ src/main/java/be/ugent/sysdes2/tracking/adapters/rest/TrackingRestController.java
@@ -3,6 +3,7 @@ package be.ugent.sysdes2.tracking.adapters.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,7 @@ import be.ugent.sysdes2.tracking.domain.Task;
 import be.ugent.sysdes2.tracking.domain.TrackingService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/tracking")
 public class TrackingRestController {
     private TrackingService trackingService;
