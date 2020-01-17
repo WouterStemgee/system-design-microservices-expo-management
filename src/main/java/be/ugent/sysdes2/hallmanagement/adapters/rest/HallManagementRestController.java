d4d0dfa7fa996b91068521c8764496609f6c48f6
Tarik <tarik.atac@ugent.be>
2020-01-17 21:23:04 +0100

    crossorigin

--- src/main/java/be/ugent/sysdes2/hallmanagement/adapters/rest/HallManagementRestController.java
+++ src/main/java/be/ugent/sysdes2/hallmanagement/adapters/rest/HallManagementRestController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +23,7 @@ import be.ugent.sysdes2.hallmanagement.persistence.HallRepository;
 import be.ugent.sysdes2.hallmanagement.persistence.HallReservationRepository;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/hall-management")
 public class HallManagementRestController {
     private static Logger logger = LoggerFactory.getLogger(HallManagementRestController.class);
